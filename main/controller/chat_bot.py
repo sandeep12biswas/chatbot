be9b49798d5df1ae444bbc28fd7846f67c71caaf
sandeep <sandeep@laptop>
2019-09-04 18:03:52 +0530

    commit chatterbot

---
+++ main/controller/chat_bot.py
@@ -0,0 +1,30 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Uncomment the following lines to enable verbose logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new ChatBot instance
+bot = ChatBot(
+    'Agent',
+    storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='mongodb://localhost:27017/chatterbot-databases'
+)
+
+trainer = ChatterBotCorpusTrainer(bot)
+
+# trainer.train(
+#     "chatterbot.corpus.english.greetings",
+#     "chatterbot.corpus.english.conversations"
+# )
+
+trainer.train(
+    "chatterbot.corpus.english.homeloan",
+    "chatterbot.corpus.english.ai",
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.conversations"
+)
